527bb81fbeaf4965eba9739c1fb70513de8af553
kaiyu li <krisfk@gmail.com>
2022-06-20 03:24:26 +0800

    .

--- wp-content/themes/cassian-lau/page-bio.php
+++ wp-content/themes/cassian-lau/page-bio.php
@@ -100,21 +100,10 @@ get_header(); ?>
         </div>
         <div class="col-2">
 
-            <ul class="side-menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="javascript:void(0);" class="parent-a">About Myself</a>
-                    <ul class="sub-menu">
-                        <li><a href="#">Bio</a></li>
-                        <li><a href="#">CV</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Exhibition</a></li>
-                <li><a href="#">Archieve</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Interview</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
+            <?php
+echo get_template_part( 'menu' );
 
+?>
         </div>
 
 
