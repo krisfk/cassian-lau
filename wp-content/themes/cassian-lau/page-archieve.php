889e3b9b01f7996c7c3c7895ba7e4aa675ffeca0
kaiyu li <krisfk@gmail.com>
2022-06-20 04:01:57 +0800

    .

--- wp-content/themes/cassian-lau/page-archieve.php
+++ wp-content/themes/cassian-lau/page-archieve.php
@@ -38,7 +38,7 @@ get_header(); ?>
 
         <div class="col-8 main-content-div">
 
-            <h1>Exhibition</h1>
+            <h1>Archieve</h1>
 
             <div class="mt-4"> Years:
             </div>
