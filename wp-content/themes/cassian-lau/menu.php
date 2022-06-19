c06e79d280f3d161bb5235e775a2cf2ef7461924
kaiyu li <krisfk@gmail.com>
2022-06-20 03:12:12 +0800

    .

---
+++ wp-content/themes/cassian-lau/menu.php
@@ -0,0 +1,14 @@
+<ul class="side-menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="javascript:void(0);" class="parent-a">About Myself</a>
+        <ul class="sub-menu">
+            <li><a href="#">Bio</a></li>
+            <li><a href="#">CV</a></li>
+        </ul>
+    </li>
+    <li><a href="#">Exhibition</a></li>
+    <li><a href="#">Archieve</a></li>
+    <li><a href="#">Blog</a></li>
+    <li><a href="#">Interview</a></li>
+    <li><a href="#">Contact</a></li>
+</ul>
